ceb98869254e7840d37eb48588c15dbda2b0f89a
mna25hope <44992665+mna25hope@users.noreply.github.com>
2019-03-25 07:04:59 +0300

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,14 @@
-		var link = document.querySelector(".button-search-hotel");
-		
-		var popup = document.querySelector(".modal-search");
-		var close = popup.querySelector(".button-search-hotel");
-		
-		link.addEventListener("click", function (evt) {
-			evt.preventDefault();
-			popup.classList.add("modal-show");
-		});
-		
-		close.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		popup.classList.remove("modal-show");
-		});
+var link = document.querySelector(".button-search-hotel");
+
+var popup = document.querySelector(".modal-search");
+var close = popup.querySelector(".button-search-hotel");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
